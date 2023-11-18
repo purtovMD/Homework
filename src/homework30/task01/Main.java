dc9f050c47be0d06860b34ac69239d68ab968f46
Maksim Purtov <Maksimgrin82@gmail.com>
2023-11-18 17:27:16 +0200

    add homework30

---
+++ src/homework30/task01/Main.java
@@ -0,0 +1,26 @@
+package homework30.task01;
+
+public class Main {
+
+  /*
+  byte
+  short
+  int
+  long
+  float
+  double
+  char
+  String
+  boolean
+   */
+  public static void main(String[] args) {
+    int num1 = 15;
+    System.out.println("Целый: " + num1 + ", 4 байта");
+
+    double num2 = 6.12;
+    System.out.println("Нецелый: " + num2 + ", 8 байт");
+
+    char letter = 'A';
+    System.out.println("Символьный: " + letter + ", 1 байт");
+  }
+}
---
+++ src/homework30/task02/Main.java
@@ -0,0 +1,14 @@
+package homework30.task02;
+
+public class Main {
+
+  public static void main(String[] args) {
+    double a = 5;
+    double b = 10;
+    double c = 9;
+    double d = -2;
+
+    double res = a * (b - d) + c * a / d;
+    System.out.println(res);
+  }
+}
