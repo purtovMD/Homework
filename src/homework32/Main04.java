5c73cf9af52770f0cef0c8690ba85956f15fe02b
Maksim Purtov <Maksimgrin82@gmail.com>
2023-11-24 13:27:31 +0200

    Fixed Main04

--- src/homework32/Main04.java
+++ src/homework32/Main04.java
@@ -10,8 +10,20 @@ public class Main04 {
     for (int i = 0; i < arr.length; i++) {
       arr[i] = scanner.nextInt();
     }
+
     for (int i = 0; i < arr.length; i++) {
       System.out.print(arr[i] + " ");
     }
+
+
+    System.out.println();
+    for (int i = 1; i < arr.length; i++) {
+      if (arr[i] < arr[i - 1]) {
+        System.out.println("Массив не отсортирован");
+        return;
+      }
+    }
+    System.out.println("Массив отсортирован");
   }
 }
+
